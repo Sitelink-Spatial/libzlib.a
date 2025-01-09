ad02bc1645aa88dbbd5bc5936220b6c7a711caa7
Robert Umbehant <github@wheresjames.com>
2025-01-09 15:58:14 +0100

    r2

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ iOS build for [zlib](https://github.com/madler/zlib)
 
     .binaryTarget(
         name: "libzlib.a",
-        url: "https://github.com/Imajion/libzlib.a/releases/download/r1/libzlib.a.xcframework.zip",
-        checksum: "ab7449eb328b69c110b5df645b8132e36366a5007349fd68ebb660d1a6b4288e"
+        url: "https://github.com/Imajion/libzlib.a/releases/download/r2/libzlib.a.xcframework.zip",
+        checksum: "4f71f1f37af8c77aceb3036af64982b7905979685d6110b5142364db3b128d80"
     )
 
 ```
--- build.sh
+++ build.sh
@@ -29,6 +29,8 @@
 # Script params
 
 LIBNAME="libzlib"
+LIBREPO="https://github.com/madler/zlib.git"
+LIBVER="v1.2.13"
 
 # What to do (build, test)
 BUILDWHAT="$1"
@@ -150,6 +152,9 @@ TGT_OPTS=
 if [[ $BUILDTARGET == *"simulator"* ]]; then
     TGT_OPTS="-simulator"
 fi
+if [[ $BUILDTARGET == *"xbuild"* ]]; then
+    TGT_OPTS="-xbuild"
+fi
 
 # NUMCPUS=1
 NUMCPUS=$(sysctl -n hw.physicalcpu)
@@ -201,8 +206,10 @@ LIBROOT="${BUILDOUT}/${BUILDTARGET}/lib3"
 LIBINST="${BUILDOUT}/${BUILDTARGET}/install"
 
 PKGNAME="${LIBNAME}.a.xcframework"
-PKGROOT="${BUILDOUT}/pkg/${PKGNAME}"
-PKGFILE="${BUILDOUT}/pkg/${PKGNAME}.zip"
+PKGBASE="${BUILDOUT}/pkg"
+PKGBUILD="${PKGBASE}/build"
+PKGROOT="${PKGBASE}/${PKGNAME}"
+PKGFILE="${PKGBASE}/${PKGNAME}.zip"
 
 # iOS toolchain
 if [[ $BUILDTARGET == *"ios"* ]]; then
@@ -230,7 +237,7 @@ if [[ $BUILDTARGET == *"ios"* ]]; then
     else
         if [ "${TGT_ARCH}" == "x86" ]; then
             TGT_PLATFORM="OS"
-        elif [ "${TGT_ARCH}" == "x86_64" ]; then
+        elif [ "${TGT_ARCH}" == *"x86_64"* ]; then
             TGT_ARCH="arm64_x86_64"
             TGT_PLATFORM="OS64COMBINED"
         else
@@ -268,6 +275,8 @@ showParams()
     echo ""
     Log "#--------------------------------------------------------------------"
     Log "LIBNAME        : ${LIBNAME}"
+    Log "LIBREPO        : ${LIBREPO}"
+    Log "LIBVER         : ${LIBVER}"
     Log "BUILDWHAT      : ${BUILDWHAT}"
     Log "BUILDTYPE      : ${BUILDTYPE}"
     Log "BUILDTARGET    : ${BUILDTARGET}"
@@ -321,14 +330,14 @@ if    [ ! -z "${REBUILDLIBS}" ] \
     # Remove existing package
     rm -Rf "${PKGROOT}/${TARGET}"
 
-    echo "\n====================== CHECKOUT ZLIB =====================\n"
+    echo "\n====================== CHECKOUT ${LIBNAME} =====================\n"
 
     INITCHECKOUT=
     if [ ! -d "${LIBBUILD}" ]; then
         INITCHECKOUT="YES"
     fi
 
-    gitCheckout "https://github.com/madler/zlib.git" "v1.2.13" "${LIBBUILD}"
+    gitCheckout "${LIBREPO}" "${LIBVER}" "${LIBBUILD}"
 
     if [ ! -z "${INITCHECKOUT}" ]; then
         sed -i '' '/# Example binaries/,$s|^|# +++ |' "${LIBBUILD}/CMakeLists.txt"
@@ -350,8 +359,8 @@ if    [ ! -z "${REBUILDLIBS}" ] \
         echo "\n==================== XCODE BUILDING ====================\n"
 
         # Get targets: xcodebuild -list -project mylib.xcodeproj
-        xcodebuild -project "${LIBBUILDOUT}/zlib.xcodeproj" \
-                   -target zlib_static \
+        xcodebuild -project "${LIBBUILDOUT}/${LIBNAME}.xcodeproj" \
+                   -target ${LIBNAME} \
                    -configuration Release \
                    -sdk iphonesimulator
         exitOnError "Failed to xbuild ${LIBNAME}"
@@ -417,30 +426,101 @@ if    [ ! -z "${REBUILDLIBS}" ] \
 
 fi
 
-
 #-------------------------------------------------------------------
 # Create full package
 #-------------------------------------------------------------------
-if [ -d "${PKGROOT}" ]; then
+if [[ $BUILDWHAT == *"mpack"* ]]; then
 
-    cd "${PKGROOT}"
+    if [ -d "${PKGROOT}" ]; then
 
-    TARGETINFO=
-    for SUB in */; do
-        echo "Adding: $SUB"
-        if [ -f "${SUB}/Info.target.plist" ]; then
-            TARGETINFO="$TARGETINFO$(cat "${SUB}/Info.target.plist")"
+        cd "${PKGROOT}"
+
+        TARGETINFO=
+        for SUB in */; do
+            echo "Adding: $SUB"
+            if [ -f "${SUB}/Info.target.plist" ]; then
+                TARGETINFO="$TARGETINFO$(cat "${SUB}/Info.target.plist")"
+            fi
+        done
+
+        if [ ! -z "$TARGETINFO" ]; then
+
+            TARGETINFO=""${TARGETINFO//$'\n'/\\n}""
+
+            cp "${ROOTDIR}/Info.plist.in" "${PKGROOT}/Info.plist"
+            sed -i '' "s|%%TARGETS%%|${TARGETINFO}|g" "${PKGROOT}/Info.plist"
+
+            cd "${PKGROOT}/.."
+
+            # Remove old package if any
+            if [ -f "${PKGFILE}" ]; then
+                rm "${PKGFILE}"
+            fi
+
+            # Create new package
+            zip -r "${PKGFILE}" "$PKGNAME" -x "*.DS_Store"
+
+            # Calculate sha256
+            openssl dgst -sha256 -r < "${PKGFILE}" | cut -f1 -d' ' > "${PKGFILE}.sha256.txt"
+
+            cd "${BUILDOUT}"
+
         fi
-    done
+    fi
 
-    if [ ! -z "$TARGETINFO" ]; then
+else
 
-        TARGETINFO=""${TARGETINFO//$'\n'/\\n}""
+    if [ -d "${PKGROOT}" ]; then
 
-        cp "${ROOTDIR}/Info.plist.in" "${PKGROOT}/Info.plist"
-        sed -i '' "s|%%TARGETS%%|${TARGETINFO}|g" "${PKGROOT}/Info.plist"
+        cd "${PKGROOT}"
 
-        cd "${PKGROOT}/.."
+        # Refresh package build directory
+        if [ -d "${PKGBUILD}" ]; then
+            rm -Rf "${PKGBUILD}"
+        fi
+        mkdir -p "${PKGBUILD}"
+
+        LIPO_CMD="lipo -create"
+        XCFR_CMD="xcodebuild -create-xcframework"
+        for SUB in */; do
+            SUB="${SUB%/}"
+            INCPATH="${SUB}/include"
+            LIBPATH="${SUB}/${LIBNAME}.a"
+            LIBBPATH="${PKGBUILD}/${LIBNAME}-${SUB}.a"
+            echo "\n=======================================================\n"
+            echo "SUB     : $SUB"
+            echo "INCPATH : $INCPATH"
+            echo "LIBPATH : $LIBPATH"
+            echo "LIBBPATH: $LIBBPATH"
+
+            if [ ! -f "${LIBPATH}" ]; then
+                exitWithError "Failed to find library: ${LIBPATH}"
+            fi
+            if [ ! -d "${INCPATH}" ]; then
+                exitWithError "Failed to find include: ${INCPATH}"
+            fi
+            cp "${LIBPATH}" "${LIBBPATH}"
+            LIPO_CMD="$LIPO_CMD ${LIBPATH}"
+            XCFR_CMD="$XCFR_CMD -library ${LIBBPATH} -headers ${INCPATH}"
+        done
+
+        LIBOUT="${PKGBUILD}/${PKGNAME}.a"
+        LIPO_CMD="$LIPO_CMD -output ${LIBOUT}"
+
+        PKGOUT="${PKGBUILD}/${PKGNAME}"
+        XCFR_CMD="$XCFR_CMD -output ${PKGOUT}"
+
+        echo "\n=======================================================\n"
+        echo "LIPO_CMD : $LIPO_CMD"
+        echo "XCFR_CMD : $XCFR_CMD"
+
+        # eval $LIPO_CMD
+        # exitOnError "Failed to create lipo"
+
+        eval $XCFR_CMD
+        exitOnError "Failed to create xcframework"
+
+        cd "${PKGBUILD}"
 
         # Remove old package if any
         if [ -f "${PKGFILE}" ]; then
@@ -456,6 +536,7 @@ if [ -d "${PKGROOT}" ]; then
         cd "${BUILDOUT}"
 
     fi
+
 fi
 
 showParams
